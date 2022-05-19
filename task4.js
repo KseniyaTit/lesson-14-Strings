628b9151d2966e67df9daac676e037c7c339a39a
asus-nb\admin <inessa134@gmail.com>
2022-05-19 16:25:32 +0300

    task 4 not finished

--- task4.js
+++ task4.js
@@ -1,12 +1,10 @@
 function func(sent, word){
     let words = sent.split(' ')
     for(let i = 0; i<sent.length; i++){
-        
-        if (i === word){
+     if (i === word){
            return (sent - word)
         }
     }
-    
 }
 
 //пока в процессе эта задача
